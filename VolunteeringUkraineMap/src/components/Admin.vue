897cedd902661e27702df23c5df79655eaf88221
Frank Gerards <info@typo3expertise.de>
2024-02-04 00:03:24 +0100

    admin panel

---
+++ VolunteeringUkraineMap/src/components/Admin.vue
@@ -0,0 +1,12 @@
+import { defineComponent, reactive } from 'vue';
+import markerData from '../assets/markers.json';
+
+<script setup>
+
+</script>
+
+<template />
+
+<style scoped>
+
+</style>
--- VolunteeringUkraineMap/src/components/Administration.vue
+++ VolunteeringUkraineMap/src/components/Administration.vue
@@ -1,13 +1,20 @@
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue';
 import markerData from '../assets/markers.json';
 
 export default defineComponent({
   name: 'Administration',
-  data() {
-    return {
-      importedMarkers: markerData,
-    };
+  setup() {
+    const state = reactive({
+      markers: [],
+    });
+    state.markers = markerData;
+    return { state };
+  },
+  methods: {
+    writeToFile(formData) {
+      console.log(`form data: ${formData}`);
+    },
   },
 });
 </script>
@@ -16,65 +23,70 @@ export default defineComponent({
   <div class="flex p-3">
     <form
       class="admin-form"
-      v-bind="importedMarkers"
     >
       <fieldset
-        v-for="(marker, markerIndex) in importedMarkers"
+        v-for="(marker, markerIndex) in state.markers"
         :key="markerIndex"
+        class="mb-12"
       >
-        <div class="mb-8">
-          <h2
-            class="mb-2 font-bold"
-          >
-            Project name: {{ marker.name }}
-          </h2>
+        <h2
+          class="font-bold"
+        >
+          Project name: {{ marker.name }}
+        </h2>
+        <p class="mt-4">
           Name:
           <input
-            class="border-2 p-1"
+            v-model="marker.name"
+            class="w-1/3 rounded-lg border-2 px-2 py-1"
             type="text"
             placeholder="marker.name"
-            :value="marker.name"
+            @change="writeToFile(marker.name)"
           >
+        </p>
+        <p
+          class="mt-4"
+        >
+          URL:
+          <input
+            v-model="marker.url"
+            class="ml-3 w-11/12 rounded-lg border-2 p-1"
+            type="text"
+          >
+        </p>
+        <div class="mx-6 my-4 w-full">
+          <h3 class="mb-2 font-semibold italic">
+            Project locations
+          </h3>
           <p
-            class="mt-3"
+            v-for="(position,posIndex) in marker.positions"
+            :key="posIndex"
+            class="mb-2 scale-90 px-4"
           >
-            URL:
+            City
             <input
-              class="w-11/12 rounded-lg border-2 p-1"
+              v-model="position.city"
+              class="mx-2 w-1/4 border-2 px-1"
               type="text"
-              :value="marker.url"
+            >Lat
+            <input
+              v-model="position.lat"
+              class="mx-2 border-2 px-1"
+              type="text"
+            >Lng
+            <input
+              v-model="position.lng"
+              class="mx-1 border-2 px-1"
+              type="text"
+              @change="writeToFile(state.markers[1])"
             >
+            <input
+              v-model="position.show"
+              type="checkbox"
+              class="mx-2 border-2"
+              :checked="position.show"
+            >Active
           </p>
-          <div class="m-6">
-            Positions
-            <p
-              v-for="(position,posIndex) in marker.positions"
-              :key="posIndex"
-              class="p-2"
-            >
-              City
-              <input
-                class="border-2 p-1"
-                type="text"
-                :value="position.city"
-              >Lat
-              <input
-                class="border-2 p-1"
-                type="text"
-                :value="position.lat"
-              >Lng
-              <input
-                class="border-2 p-1"
-                type="text"
-                :value="position.lng"
-              >
-              <input
-                type="checkbox"
-                class="mx-2 border-2"
-                :checked="position.show"
-              >Active
-            </p>
-          </div>
         </div>
       </fieldset>
     </form>
